f6a9439dc7680dd18750429fcf195b31418ceb9c
bridger-huhn <57768072+bridger-huhn@users.noreply.github.com>
2025-04-16 13:25:00 -0600

    deleted shape

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-// Initialize the map centered somewhere
-var map = L.map('map').setView([40.596395, -105.071878], 13); // Innosphere Coords
-
-// Add Esri satellite basemap
-L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-  attribution: 'Tiles © Esri'
-}).addTo(map);
-
-// Example GeoJSON overlay (optional)
-fetch('data.geojson')
-  .then(response => response.json())
-  .then(data => {
-    L.geoJSON(data, {
-      style: {
-        color: 'red',
-        weight: 2,
-        fillOpacity: 0.3
-      }
-    }).addTo(map);
-  });
-
-
-  var imageBounds = [[40.59, -105.08], [40.61, -105.06]];
-L.imageOverlay('overlay.png', imageBounds).addTo(map);
 
 // Initialize the map centered on Colorado
 var map = L.map('map').setView([39.5501, -105.7821], 7); // Center of Colorado
