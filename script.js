3ee69f8092f5c8458163cec5ffa5476281c2198f
bridger-huhn <57768072+bridger-huhn@users.noreply.github.com>
2025-04-16 13:13:25 -0600

    added sample heat map

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Initialize the map centered somewhere
-var map = L.map('map').setView([40.596395, -105.071878], 13); // New York City coords
+var map = L.map('map').setView([40.596395, -105.071878], 13); // Innosphere Coords
 
 // Add Esri satellite basemap
 L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
@@ -22,3 +22,24 @@ fetch('data.geojson')
 // Example image overlay (optional)
 var imageBounds = [[40.70, -74.02], [40.72, -73.99]];
 L.imageOverlay('overlay.png', imageBounds).addTo(map);
+
+
+// Example heatmap points: [lat, lng, intensity (optional)]
+var heatData = [
+    [40.596395, -105.071878, 0.5],
+    [40.600000, -105.065000, 0.8],
+    [40.590000, -105.080000, 0.3]
+  ];
+  
+  // Create the heatmap layer
+  var heat = L.heatLayer(heatData, {
+    radius: 25,
+    blur: 15,
+    maxZoom: 17,
+    gradient: {
+      0.4: 'blue',
+      0.65: 'lime',
+      1: 'red'
+    }
+  }).addTo(map);
+  
